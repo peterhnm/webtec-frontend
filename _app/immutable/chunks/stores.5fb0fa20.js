43c69c3c21ef855e325a4d801180a6fc4d0baa1c
peterhnm <peter.hnm@gmx.de>
2023-05-10 14:41:05 +0200

    Updates

---
+++ _app/immutable/chunks/stores.5fb0fa20.js
@@ -0,0 +1,1 @@
+import"./index.649217a0.js";import{s as e}from"./singletons.fefd1d98.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
