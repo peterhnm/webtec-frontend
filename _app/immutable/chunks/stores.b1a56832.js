1cca5d7f75be867e01c31059c306bf11429e0b53
peterhnm <peter.hnm@gmx.de>
2023-05-23 18:09:50 +0200

    Updates

--- _app/immutable/chunks/stores.b1a56832.js
+++
@@ -1,1 +0,0 @@
-import{j as t,w as a}from"./singletons.c423b79a.js";const s=t("goto"),e=t("after_navigate"),r=a(),g=a();export{e as a,s as g,r as p,g as t};
---
+++ _app/immutable/chunks/stores.f13b1a6b.js
@@ -0,0 +1,1 @@
+import{j as t,w as a}from"./singletons.2b5d4684.js";const s=t("goto"),e=t("after_navigate"),r=a(),g=a();export{e as a,s as g,r as p,g as t};
