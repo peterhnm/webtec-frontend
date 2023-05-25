20b1c2ae9aa77081075a5c1d8c23b0ac7807e942
peterhnm <peter.hnm@gmx.de>
2023-05-25 20:04:13 +0200

    Updates

---
+++ _app/immutable/chunks/navigation.6c9af8ff.js
@@ -0,0 +1,1 @@
+import{j as t}from"./singletons.e6e9b06c.js";const o=t("goto"),e=t("after_navigate");export{e as a,o as g};
---
+++ _app/immutable/chunks/stores.9429585d.js
@@ -0,0 +1,1 @@
+import{w as t}from"./singletons.e6e9b06c.js";const s=t(),e=t([]),r=t();export{r as h,s as p,e as s};
