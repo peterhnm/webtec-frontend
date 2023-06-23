581f386aae825a80705412d6ff82a8a9deaf8650
peterhnm <peter.hnm@gmx.de>
2023-06-23 10:07:00 +0200

    fix: formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -40,16 +40,17 @@
             if (!selectedTags) {
                 return;
             }
+
             // eslint-disable-next-line no-undef
             const selectedBoxes: NodeListOf<HTMLInputElement> =
-                selectedTags.querySelectorAll("input[type=\"checkbox\"]");
+                selectedTags.querySelectorAll('input[type="checkbox"]');
             for (const box of selectedBoxes) {
                 box.checked = true;
             }
 
             // eslint-disable-next-line no-undef
             const dropdownBoxes: NodeListOf<HTMLInputElement> =
-                dropdown.querySelectorAll("input[type=\"checkbox\"]");
+                dropdown.querySelectorAll('input[type="checkbox"]');
             for (const box of dropdownBoxes) {
                 box.checked = false;
             }
