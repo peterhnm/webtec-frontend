09d4574f4958122d5ec91106a6dcb4fdd64b955d
peterhnm <peter.hnm@gmx.de>
2023-06-26 22:33:16 +0200

    Updates

---
+++ _app/immutable/chunks/paths.02bf2e9e.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_mrg8bi)==null?void 0:s.base)??"/webtec-frontend";var e;const a=((e=globalThis.__sveltekit_mrg8bi)==null?void 0:e.assets)??t;export{a,t as b};
