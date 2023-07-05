5f282b063cb421ede5769a5ecf71911d1f60f87f
peterhnm <peter.hnm@gmx.de>
2023-07-05 19:56:30 +0200

    Updates

--- _app/immutable/chunks/paths.4f45f2c5.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_1tytjyy)==null?void 0:s.base)??"/webtec-frontend";var t;const a=((t=globalThis.__sveltekit_1tytjyy)==null?void 0:t.assets)??e;export{a,e as b};
---
+++ _app/immutable/chunks/paths.c77deb4a.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_7tdct5)==null?void 0:s.base)??"/webtec-frontend";var t;const a=((t=globalThis.__sveltekit_7tdct5)==null?void 0:t.assets)??e;export{a,e as b};
